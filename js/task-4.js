22e6301c9f5c11a8375e56605c86a3f435e9cb26
Natalia <164662428+Natalia-Honcharova@users.noreply.github.com>
2026-04-26 19:11:01 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = e.target.elements.email.value.trim();
+  const password = e.target.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const data = {
+    email,
+    password,
+  };
+
+  console.log(data);
+
+  form.reset();
+});
